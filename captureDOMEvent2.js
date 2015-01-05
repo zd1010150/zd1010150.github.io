81421ebfed58abbdd63bac8cc122faaf0eae04a9
zd1010150 <zd1010150@gmail.com>
2015-01-05 21:18:40 +0800

    Update captureDOMEvent2.js

--- captureDOMEvent2.js
+++ captureDOMEvent2.js
@@ -103,7 +103,6 @@ var XML=window.XML||{
 function addWindowToPage(){
 	var windowHTML='<div id="_plugInselectWindow" class="easyui-window" data-options="closed:true,modal:false,title:\'Test Window\'" style="width:300px;height:100px;"></div>';
    document.body.innerHTML+=windowHTML;
-    $("#_plugInselectWindow").window(
-    	"open");
+    $("#_plugInselectWindow").window("open");
 }
 addWindowToPage();
